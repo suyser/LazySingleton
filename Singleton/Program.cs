23d65e295d2deaafaaf01c843770809d8454439d
suyser <zagorulkokristina554@gmail.com>
2024-09-15 23:33:33 +0700

    init

---
+++ Singleton/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Servers servers = Servers.Instance;
+
+        Console.WriteLine(servers.AddServer("http://example.com"));
+        Console.WriteLine(servers.AddServer("https://example.com"));
+        Console.WriteLine(servers.AddServer("http://example.com")); // False, потому что дубликат
+        Console.WriteLine(servers.AddServer("ftp://example.com")); // False, потому что не http/https
+
+        Console.WriteLine("HTTP servers: " + string.Join(", ", servers.GetHttpServers()));
+        Console.WriteLine("HTTPS servers: " + string.Join(", ", servers.GetHttpsServers()));
+    }
+}
---
+++ Singleton/Servers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Servers
+{
+    private static readonly Servers _instance = new Servers();
+    private HashSet<string> _servers;
+
+    private Servers()
+    {
+        _servers = new HashSet<string>();
+    }
+
+    public static Servers Instance => _instance;
+
+    public bool AddServer(string server)
+    {
+        if (!server.StartsWith("http://") && !server.StartsWith("https://"))
+        {
+            return false;
+        }
+
+        lock (_servers)
+        {
+            return _servers.Add(server);
+        }
+    }
+
+    public List<string> GetHttpServers()
+    {
+        lock (_servers) 
+        {
+            return new List<string>(_servers.Where(s => s.StartsWith("http://")));
+        }
+    }
+
+    public List<string> GetHttpsServers()
+    {
+        lock (_servers) 
+        {
+            return new List<string>(_servers.Where(s => s.StartsWith("https://")));
+        }
+    }
+}
